f3e68127eb585a5fec897c411bf1fae23bb4ac57
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-15 16:48:11 +0000

    initiate play.js, connect to snek server

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+
+    host: 'localhost',  // IP address here,
+    port: 50541,        // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+connect().on("data", (data) => {
+  console.log("Snek says: ", data);
+});
