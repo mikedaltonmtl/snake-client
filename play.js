b2500f50aaa4bf040ceedbedcb4d509fae3feb43
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-16 12:04:45 +0000

    add name function to play.js

--- play.js
+++ play.js
@@ -1,8 +1,17 @@
 const connect = require('./client.js');
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
 
-connect().on("data", (data) => {
+conn.on("data", (data) => {
   console.log("Snek says: ", data);
+});
+
+conn.on("connect", () => {
+
+  console.log("Successfully connected to game server");
+
+  conn.write("Name: MJD");
+
+
 });
