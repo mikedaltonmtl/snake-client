76c600d84fc103830554f7f2746b9549cad06ebf
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-16 13:36:30 +0000

    add event listener to play.js for ctrl+c to quit game

--- play.js
+++ play.js
@@ -1,19 +1,19 @@
-const connect = require('./client.js');
+const connect = require('./client.js'); // to establish connection with game server
 
 console.log("Connecting ...");
-const conn = connect();
+const conn = connect(); // establish the connection
 
+// print output received from the game server
 conn.on("data", (data) => {
   console.log("Snek says: ", data);
 });
 
+// instructions upon connection
 conn.on("connect", () => {
 
   console.log("Successfully connected to game server");
 
-  conn.write("Name: MJD");
-
-  
+  conn.write("Name: MJD"); // transmit to game server
 });
 
 conn.on("connect", () => {
@@ -24,5 +24,28 @@ conn.on("connect", () => {
   //     conn.write("Move: up");
   //   }, 50 * i) 
   // }
-
 });
+
+// setup interface to handle user input from stdin
+const setupInput = function() {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); // add event listener
+
+  return stdin;
+};
+
+// event handler
+const handleUserInput = function(key) {
+
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+};
+
+setupInput();
