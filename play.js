607d9859ebae37f96d5f9a8f00793d59cf023bd3
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-16 13:48:47 +0000

    refactor play.js, modularize user input by moving setupInput() and handleUserInput() to new file input.js

--- play.js
+++ play.js
@@ -1,4 +1,5 @@
 const connect = require('./client.js'); // to establish connection with game server
+const setupInput = require('./input.js'); // interface to handle user input from stdin
 
 console.log("Connecting ...");
 const conn = connect(); // establish the connection
@@ -10,42 +11,19 @@ conn.on("data", (data) => {
 
 // instructions upon connection
 conn.on("connect", () => {
-
   console.log("Successfully connected to game server");
-
   conn.write("Name: MJD"); // transmit to game server
 });
 
-conn.on("connect", () => {
 
+
+setupInput(); // user input interface
+
+
+// conn.on("connect", () => {
   // for (let i = 1; i < 40; i++) {
-
   //   setTimeout(() => {
   //     conn.write("Move: up");
   //   }, 50 * i) 
   // }
-});
-
-// setup interface to handle user input from stdin
-const setupInput = function() {
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput); // add event listener
-
-  return stdin;
-};
-
-// event handler
-const handleUserInput = function(key) {
-
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-
-};
-
-setupInput();
+// });
