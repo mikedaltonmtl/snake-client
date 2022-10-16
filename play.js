241944b3491a3d9e0078bbc5db908818c61abf5f
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-16 00:21:19 +0000

    move connect function to client.js file

--- play.js
+++ play.js
@@ -1,18 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-
-    host: 'localhost',  // IP address here,
-    port: 50541,        // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const connect = require('./client.js');
 
 console.log("Connecting ...");
 connect();
